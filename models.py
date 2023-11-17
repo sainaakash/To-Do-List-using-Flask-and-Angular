bdac876d0b4b5b6a536e753a43df9fe2b7b73d23
sainaakash <aakash.sain08@gmail.com>
2023-11-17 16:20:53 +0530

    app_json.py and models.py created

--- models.py
+++ models.py
@@ -2,11 +2,10 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-class ProductModel(db.Model):
+class TasksModel(db.Model):
     __tablename__ = 'products'
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.String(255))
-    
 
     def __init__(self,content):
         self.content = content
